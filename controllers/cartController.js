61c05716fa4617d003efbda7031cdb62c5514dfa
Caleb Muturi <calebmuturi2024@gmail.com>
2025-07-29 09:42:55 +0300

    feat: implement order placement functionality

--- controllers/cartController.js
+++ controllers/cartController.js
@@ -3,16 +3,17 @@ import userModel from '../models/userModel.js';
 // add items to user cart
 const addToCart = async (req, res) => {
   try {
-    let userData = await userModel.findOne({
-      _id: req.body.userId,
-    });
-    let cartData = await userData.cartData;
+    let userData = await userModel.findById(req.userId);
+    if (!userData) {
+      return res.json({ success: false, message: 'User not found' });
+    }
+    let cartData = userData.cartData || {};
     if (!cartData[req.body.itemId]) {
       cartData[req.body.itemId] = 1;
     } else {
       cartData[req.body.itemId] += 1;
     }
-    await userModel.findByIdAndUpdate(req.body.userId, { cartData });
+    await userModel.findByIdAndUpdate(req.userId, { cartData });
     res.json({
       success: true,
       message: 'Added To Cart',
@@ -29,12 +30,15 @@ const addToCart = async (req, res) => {
 // remove items from user cart
 const removeFromCart = async (req, res) => {
   try {
-    let userData = await userModel.findById(req.body.userId);
-    let cartData = await userData.cartData;
+    let userData = await userModel.findById(req.userId);
+    if (!userData) {
+      return res.json({ success: false, message: 'User not found' });
+    }
+    let cartData = userData.cartData || {};
     if (cartData[req.body.itemId] > 0) {
       cartData[req.body.itemId] -= 1;
     }
-    await userModel.findByIdAndUpdate(req.body.userId, { cartData });
+    await userModel.findByIdAndUpdate(req.userId, { cartData });
     res.json({
       success: true,
       message: 'Removed From Cart',
@@ -52,7 +56,10 @@ const removeFromCart = async (req, res) => {
 const getCart = async (req, res) => {
     try {
         let userData = await userModel.findById(req.userId);
-        let cartData = await userData.cartData;
+        if (!userData) {
+          return res.json({ success: false, message: 'User not found' });
+        }
+        let cartData = userData.cartData || {};
         res.json({ success: true, cartData })
     } catch (error) {
         console.log(error);
